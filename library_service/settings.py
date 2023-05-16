a1043c6b029a26a81384870021ffac9d0a616fe2
Arsen Makovei <makoveyarsen@gmail.com>
2023-05-16 20:58:00 +0300

    Added new app users_service, created user model with email

--- library_service/settings.py
+++ library_service/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -100,6 +101,8 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+AUTH_USER_MODEL = "users.User"
 
 
 # Internationalization
