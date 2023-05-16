f04085b2db567e13c546b85a72f177ddad3bbd07
Arsen Makovei <makoveyarsen@gmail.com>
2023-05-16 21:54:28 +0300

    Added user views with create, retrieve and update options

--- library_service/settings.py
+++ library_service/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "rest_framework_simplejwt",
     "users",
 ]
 
@@ -110,7 +112,7 @@ AUTH_USER_MODEL = "users.User"
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Kiev"
 
 USE_I18N = True
 
