f3ac3434ffc27990721862f3213aecf20c603625
Arsen Makovei <makoveyarsen@gmail.com>
2023-05-19 22:48:50 +0300

    Updated inventory field in book model, made db migrations

--- books/models.py
+++ books/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 
@@ -9,7 +10,7 @@ class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     cover = models.CharField(max_length=50, choices=CoverChoices.choices)
-    inventory = models.PositiveIntegerField()
+    inventory = models.IntegerField(validators=[MinValueValidator(0)])
     dayle_fee = models.DecimalField(max_digits=8, decimal_places=2)
 
     def __str__(self):
