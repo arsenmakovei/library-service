84d2559a45b2756a230fbe83430eb7b02942ea9f
Arsen Makovei <makoveyarsen@gmail.com>
2023-05-22 23:10:21 +0300

    Created payment model, made db migrations, registered model in admin site

--- borrowings/admin.py
+++ borrowings/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from borrowings.models import Borrowing
-
+from borrowings.models import Borrowing, Payment
 
 admin.site.register(Borrowing)
+admin.site.register(Payment)
--- borrowings/models.py
+++ borrowings/models.py
@@ -17,3 +17,25 @@ class Borrowing(models.Model):
 
     def __str__(self):
         return f"{self.user.email}: {self.book.title}"
+
+
+class Payment(models.Model):
+    class StatusChoices(models.TextChoices):
+        PENDING = "Pending"
+        PAID = "Paid"
+
+    class TypeChoices(models.TextChoices):
+        PAYMENT = "Payment"
+        FINE = "Fine"
+
+    status = models.CharField(max_length=50, choices=StatusChoices.choices)
+    type = models.CharField(max_length=50, choices=TypeChoices.choices)
+    borrowing = models.ForeignKey(
+        Borrowing, on_delete=models.CASCADE, related_name="payments"
+    )
+    session_url = models.URLField(null=True, blank=True)
+    session_id = models.CharField(max_length=255, null=True, blank=True)
+    money_to_pay = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return f"Payment #{self.id}"
