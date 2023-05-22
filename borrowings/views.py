fbe09876e512e68f0d10ef6428b961dddbe6eeef
Arsen Makovei <makoveyarsen@gmail.com>
2023-05-22 22:45:22 +0300

    Created views for borrowing model

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,76 @@
-from django.shortcuts import render
+from django.utils import timezone
+from rest_framework import mixins, viewsets, status
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+from borrowings.models import Borrowing
+from borrowings.serializers import (
+    BorrowingSerializer,
+    BorrowingListSerializer,
+    BorrowingReturnSerializer,
+)
+
+
+class BorrowingViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.CreateModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Borrowing.objects.select_related("book", "user")
+    serializer_class = BorrowingSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = self.queryset
+        is_active = self.request.query_params.get("is_active")
+        user_id = self.request.query_params.get("user_id")
+
+        if is_active:
+            if is_active.lower() == "true":
+                queryset = queryset.filter(actual_return_date=None)
+            else:
+                queryset = queryset.exclude(actual_return_date=None)
+
+        if self.request.user.is_staff:
+            if user_id:
+                queryset = queryset.filter(user__id=user_id)
+
+            return queryset
+
+        return queryset.filter(user=self.request.user)
+
+    def get_serializer_class(self):
+        if self.action in ["list", "retrieve"]:
+            return BorrowingListSerializer
+
+        if self.action == "book_return":
+            return BorrowingReturnSerializer
+
+        return BorrowingSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    @action(methods=["POST"], detail=True, url_path="return")
+    def book_return(self, request, pk=None):
+        borrowing = self.get_object()
+
+        if borrowing.actual_return_date is not None:
+            return Response(
+                {"error": "Book has already been returned"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        borrowing.actual_return_date = timezone.now()
+        borrowing.save()
+
+        book = borrowing.book
+        book.inventory += 1
+        book.save()
+
+        return Response(
+            {"success": "Your book was successfully returned"},
+            status=status.HTTP_200_OK,
+        )
