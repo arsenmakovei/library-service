74f58dbce892cac0bd00823b5df26ba6edc00f9e
Arsen Makovei <makoveyarsen@gmail.com>
2023-05-24 23:30:37 +0300

    Created payment serializer, added payment on borrowing creation

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -1,10 +1,28 @@
+from django.db import transaction
 from rest_framework import serializers
 
 from books.serializers import BookSerializer
-from borrowings.models import Borrowing
+from borrowings.models import Borrowing, Payment
+from borrowings.payment_service import create_stripe_session
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = (
+            "id",
+            "status",
+            "type",
+            "borrowing",
+            "session_url",
+            "session_id",
+            "money_to_pay",
+        )
 
 
 class BorrowingSerializer(serializers.ModelSerializer):
+    payments = PaymentSerializer(many=True, read_only=True)
+
     class Meta:
         model = Borrowing
         fields = (
@@ -14,6 +32,7 @@ class BorrowingSerializer(serializers.ModelSerializer):
             "actual_return_date",
             "book",
             "user",
+            "payments",
         )
         read_only_fields = ("id", "borrow_date", "actual_return_date", "user")
 
@@ -24,14 +43,19 @@ class BorrowingSerializer(serializers.ModelSerializer):
             )
         return book
 
+    @transaction.atomic
     def create(self, validated_data):
         book = validated_data["book"]
         user = self.context["request"].user
+
         borrowing = Borrowing.objects.create(
             expected_return_date=validated_data["expected_return_date"],
             book=book,
             user=user,
         )
+
+        create_stripe_session(self.context["request"], borrowing)
+
         book.inventory -= 1
         book.save()
         return borrowing
